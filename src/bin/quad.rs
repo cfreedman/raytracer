79c338f32bc49abe36da621b5fb3f1d54cd1866d
Collum <collum.freedman@gmail.com>
2024-12-30 04:30:47 -0500

    finish nasty normalization bugs in quad implementation and hit detection - pay attention to the distinction between the use of the normalized and unnormalized normal vector in the algebra

---
+++ src/bin/quad.rs
@@ -0,0 +1,34 @@
+use raytracer::{camera::Camera, hittable::HittableList, material::{Lambertian, Material}, quad::Quad, vec3::Vec3};
+
+fn main() {
+    let mut world = HittableList::default();
+
+    // Materials
+    let left_red = Material::Lambertian(Lambertian::new_from_color(Vec3::new(1.,0.2,0.2)));
+    let back_green = Material::Lambertian(Lambertian::new_from_color(Vec3::new(0.2,1.,0.2)));
+    let right_blue = Material::Lambertian(Lambertian::new_from_color(Vec3::new(0.2,0.2,1.)));
+    let upper_orange = Material::Lambertian(Lambertian::new_from_color(Vec3::new(1.,0.5,0.)));
+    let lower_teal = Material::Lambertian(Lambertian::new_from_color(Vec3::new(0.2,0.8,0.8)));
+
+    // Quads
+    world.add(Box::new(Quad::new(Vec3::new(-3.,-2.,5.),Vec3::new(0.,0.,-4.),Vec3::new(0.,4.,0.),left_red)));
+    world.add(Box::new(Quad::new(Vec3::new(-2.,-2.,0.),Vec3::new(4.,0.,0.),Vec3::new(0.,4.,0.),back_green)));
+    world.add(Box::new(Quad::new(Vec3::new(3.,-2.,1.),Vec3::new(0.,0.,4.),Vec3::new(0.,4.,0.),right_blue)));
+    world.add(Box::new(Quad::new(Vec3::new(-2.,3.,1.),Vec3::new(4.,0.,0.),Vec3::new(0.,0.,4.),upper_orange)));
+    world.add(Box::new(Quad::new(Vec3::new(-2.,-3.,5.),Vec3::new(4.,0.,0.),Vec3::new(0.,0.,-4.),lower_teal)));
+
+    // Camera
+    let camera = Camera::new(
+        1.,
+        400,
+        100,
+        50,
+        80.,
+        Vec3::new(0.,0.,9.),
+        Vec3::new(0., 0., 0.),
+        Vec3::new(0., 1., 0.),
+        0.,
+        10.,
+    );
+    camera.render(&world);
+}
